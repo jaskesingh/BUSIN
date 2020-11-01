f78cda43dcc3155975c47c0fadac2a730b42d239
LienHegge <lien.hegge@student.uhasselt.be>
2020-11-01 22:16:02 +0100

    Dashboardlook2

--- Dashboard_Lien/server.R
+++ Dashboard_Lien/server.R
@@ -8,25 +8,22 @@
 #
 
 library(shiny)
-<<<<<<< HEAD
 library(readxl)
 library(ggplot2)
 library(tidyr)
-Revenue <- read_xlsx("data/Revenue-gross margin-gross profit worldwide 2015-2020.xlsx", sheet = "Revenues (automotive)", col_types = c("text", "text", "numeric", "numeric"))
-
-=======
 library(shinydashboard)
-library(ggplot2)
 library(dplyr)
 library(tidyr)
 library(lubridate)
 library(readr)
+Revenue <- read_xlsx("data/Revenue-gross margin-gross profit worldwide 2015-2020.xlsx", sheet = "Revenues (automotive)", col_types = c("text", "text", "numeric", "numeric"))
+
+
 
 revenue <- function(yearinput,df) {
   revenue <- df %>% filter(df$Year == yearinput)
   return(revenue)
 }
->>>>>>> b651ac5c49c0b17ec379b740337dca0473d51c5d
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
--- Dashboard_Lien/ui.R
+++ Dashboard_Lien/ui.R
@@ -11,6 +11,11 @@ library(shiny)
 library(readxl)
 library(ggplot2)
 library(tidyr)
+library(shinydashboard)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(readr)
 Revenue <- read_xlsx("data/Revenue-gross margin-gross profit worldwide 2015-2020.xlsx", sheet = "Revenues (automotive)", col_types = c("numeric", "text", "numeric", "numeric"))
 
 # Define UI for application that draws a histogram
