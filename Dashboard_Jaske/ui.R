02dfa2284370f0fe5c6c47b3b6aa544d67a7243f
Jaske Singh <jaske163@gmail.com>
2020-11-02 21:38:09 +0100

    baby steps

--- Dashboard_Jaske/ui.R
+++ Dashboard_Jaske/ui.R
@@ -11,6 +11,7 @@ eusurvey <- read.csv('data/hev1.csv')
 
 shinyUI(
   dashboardPage(
+    skin = 'red',
     dashboardHeader(title = "TEST",
                     dropdownMenu(
                       type = "tasks",
@@ -24,8 +25,9 @@ shinyUI(
     dashboardSidebar(
       sidebarMenu(
         menuItem("Klanten", tabName = "klanten",
-                 menuItem("Test", tabName = "test"),
-                 menuItem("Test1", tabName = "test1"))
+                 menuSubItem("Test", tabName = "test"),
+                 menuSubItem("Test1", tabName = "test1"),
+                 menuSubItem("Income", tabName = "income"))
       )
     ),
     
@@ -39,24 +41,36 @@ shinyUI(
                   tabPanel("Country based",
                            selectInput(inputId = "gcountry",
                                        label = "Choose country",
-                                       choices = levels(hev1$Country),
+                                       choices = levels(eusurvey$Country),
                                        multiple = T,
                                        selected = "Belgium"),
                            plotOutput("gcountry"))
             
-                  ),
-                tabItem(tabName = "test1",
+                  )),
+        tabItem(tabName = "test1",
                 tabBox(
                   title = "per country",
                   tabPanel(" ", selectInput(inputId = "country",
                                   label = "Choose country",
-                                  choices = levels(hev1$Country),
-                                  multiple = T,
-                                  selected = "Belgium")),
-                  dataTableOutput("country"))
-    )
+                                  choices = levels(eusurvey$Country),
+                                  selected = "Belgium",
+                                  multiple = T),
+                           dataTableOutput("country")), width = 8)
+                ),
+        tabItem(tabName = "income",
+                tabBox(
+                  title = "based on income",
+                  tabPanel("tab1", selectInput(inputId = "incountry",
+                                               label = "choose Country",
+                                               choices = levels(eusurvey$Country),
+                                               selected = "Belgium"),
+                           selectInput(inputId = "incomegr",
+                                       label = "choose income group",
+                                       choices = levels(eusurvey$Income_group),
+                                       selected = "middle"),
+                           plotOutput("view"))
+                ))
     )
     )
   )
-)
 )
