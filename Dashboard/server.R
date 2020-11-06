9a58e7a672389e93abb3df11b1ab6d7eb50ba44d
carohaesen <caro.haesen@student.uhasselt.be>
2020-11-06 22:04:47 +0100

    libraries toegevoegd

--- Dashboard/server.R
+++ Dashboard/server.R
@@ -1,27 +1,23 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+library(readxl)
+library(maps)
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+library(lubridate)
+library(ggmap)
+library(mapdata)
+library(mapproj)
+library(sf)
+library(rnaturalearth)
+library(hrbrthemes)
+library(grid)
+library(rworldmap)
 library(shiny)
+library(plotly)
+library(leaflet)
+library(DT)
+library(rvest)
+library(stringr)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-    })
-
-})
-#
