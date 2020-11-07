bb2283fc1a4392db3964b53aae9e76383dc8b979
LienHegge <lien.hegge@student.uhasselt.be>
2020-11-07 15:53:43 +0100

    Toevoeging stuk Lien

--- Dashboard/server.R
+++ Dashboard/server.R
@@ -1,4 +1,5 @@
 library(readxl)
+library(shinydashboard)
 library(maps)
 library(ggplot2)
 library(tidyr)
@@ -13,11 +14,13 @@ library(hrbrthemes)
 library(grid)
 library(rworldmap)
 library(shiny)
+library(shinydashboard)
 library(plotly)
 library(leaflet)
 library(DT)
 library(rvest)
 library(stringr)
+
 
 #Caro
 
@@ -102,6 +105,46 @@ data <- read_xlsx("Data/Monthly Tesla Vehicle Sales.xlsx")
 Data <- data %>% gather(January:December, key=  "Month", value="Sales") %>% mutate(Month = str_replace(Month, "January", "1"), Month = str_replace(Month, "February", "2"), Month = str_replace(Month, "March", "3"), Month = str_replace(Month, "April", "4"), Month = str_replace(Month, "May", "5"), Month = str_replace(Month, "June", "6"), Month = str_replace(Month, "July", "7"), Month = str_replace(Month, "August", "8"), Month = str_replace(Month, "September", "9"), Month = str_replace(Month, "October", "10"), Month = str_replace(Month, "November", "11"), Month = str_replace(Month, "December", "12"))
 Data$Month <- as.integer(Data$Month)
 Data$Year <- as.factor(Data$Year) 
+
+
+#Lien
+
+#financiele cijfers
+Revenue <- read_xlsx("data/Revenue-gross margin-gross profit worldwide 2015-2020.xlsx", sheet = "Revenues (automotive)", col_types = c("numeric", "text", "numeric", "numeric"))
+Gross_Margin <- read_xlsx("Data/Revenue-gross margin-gross profit worldwide 2015-2020.xlsx", sheet = "Gross margin", col_types = c("numeric", "text", "numeric", "numeric"))
+Gross_profit <- read_xlsx("Data/Revenue-gross margin-gross profit worldwide 2015-2020.xlsx", sheet = "Gross profit", col_types = c("numeric", "text", "numeric", "numeric", "numeric"))
+Free_cashflow <- read_xlsx("Data/Tesla's free cash flow by quarter 2020 world wide.xlsx", skip = 3 , sheet = "Data", col_types = c("numeric", "text", "numeric"))
+#uitbreiding europa
+countriesafpassengercars <- read_xlsx("Data/Countries overview of af passenger cars.xlsx", skip = 2 , col_types = c("numeric", "text", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+countriesafinfrastructure <- read_xlsx("Data/countries overview of af infrastructure.xlsx", skip = 2 , col_types = c("numeric", "text", "numeric", "numeric", "numeric", "numeric", "numeric"))
+#financiele cijfers, functies
+revenue <- function(yearinput,df) {
+  revenue <- df %>% filter(df$Year == yearinput)
+  return(revenue)
+}
+
+
+#uitbreiding europa, data in juiste vorm krijgen
+countriesafpassengercars <- countriesafpassengercars %>% gather('BEV', 'H2', 'CNG', 'LNG', 'PHEV', 'LPG', 'Total', key = 'Fuel', value = 'waardes')
+countriesafpassengercars$Country[1:2457] <- c('Ausria', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus', 'Czech Republic', 'Denmark', 'Estonia',
+                                              'Finland', 'France', 'Germany', 'Greece', 'Hungria', 'Ireland', 'Italy', 'Latvia', 'Lithuania',
+                                              'Luxembourg', 'Malta', 'Netherlands', 'Poland', 'Portugal', 'Romania', 'Slovakia', 'Slovenia',
+                                              'Spain', 'Sweden')
+countriesafinfrastructure <- countriesafinfrastructure %>% gather('Electricity', 'H2', 'Natural Gas', 'LPG', 'Total', key = 'Fuel', value = 'waardes')
+countriesafinfrastructure$Country[1:1755] <- c('Ausria', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus', 'Czech Republic', 'Denmark', 'Estonia',
+                                               'Finland', 'France', 'Germany', 'Greece', 'Hungria', 'Ireland', 'Italy', 'Latvia', 'Lithuania',
+                                               'Luxembourg', 'Malta', 'Netherlands', 'Poland', 'Portugal', 'Romania', 'Slovakia', 'Slovenia',
+                                               'Spain', 'Sweden')
+
+#wereldkaart
+
+teslapercountrysales <- read_xlsx("Data/Verkoop landen tesla.xlsx", skip = 1, col_types = c("text", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")) %>% gather('2013', '2014', '2015', '2016', '2017', '2018', '2019', key = 'jaar', value = 'waarde')
+
+some.eu.countries <- c('Ukraine', 'France', 'Spain', 'Sweden', 'Norway', 'Germany', 'Finland', 'Poland', 'Italy', 'UK', 'Romania', 'Belarus', 'Greece', 'Bulgaria', 'Iceland', 'Hungary', 'Portugal', 'Austria', 'Czech Republic', 'Serbia', 'Ireland', 'Lithuania', 'Latvia', 'Croatia', 'Bosnia and Herzegovina', 'Slovakia', 'Estonia', 'Denmark', 'Netherlands', 'Switzerland', 'Moldova', 'Belgium', 'Armenia', 'Albania', 'Macedonia', 'Turkey', 'Slovenia', 'Montenegro', 'Kosovo', 'Cyprus', 'Luxembourg', 'Georgia', 'Andorra', 'Malta', 'Liechtenstein', 'San Marino', 'Monaco', 'Vatican')
+
+some.eu.map <- map_data("world", region = some.eu.countries)
+tesla.eu.map <- left_join(some.eu.map, teslapercountrysales, by = "region")
+
 
 # Define server logic required to draw a map
 shinyServer(function(input, output, session) {
@@ -308,6 +351,243 @@ shinyServer(function(input, output, session) {
     h8 <- DataC2 %>% ggplot(aes(x = Month, y = Sales, na.rm = T)) + geom_col() + facet_wrap(Year~.) + labs(title = "Periodic Tesla sales over the years.") + theme(axis.text.x = element_text(angle = 45, hjust = 1)) + scale_x_continuous(breaks = seq(0,12, by = 1)) +
       scale_y_continuous(limits = c(0, 25000), breaks = seq(0,25000, by= 5000)) + ylab("Sales")
     ggplotly(h8)})
+  
+  
+  #financieel tabblad
+  sortofgraph <- reactive({input$Quarterly})
+  
+  output$revbox <- renderValueBox({
+    if (sortofgraph() == TRUE) {
+      valueBox(
+        paste0(format(round(sum(Revenue$`Automotive Revenues Tesla`[Revenue$Year == input$Yearrev], na.rm = TRUE)/1000000, 2), decimal.mark = ",", big.mark = " ", small.mark = " ", small.interval = 3)),
+        subtitle= paste0("Revenue ", input$Yearrev, " in million"), 
+        icon = icon("dollar-sign"), color = 'red'
+      )
+    }
+    else {
+      
+      somjaren <- Revenue %>% filter(Year >= min(input$Yearrevline) & Year <= max(input$Yearrevline)) %>% group_by(Year) %>% summarize(totaal = sum(`Automotive Revenues Tesla`, na.rm = TRUE))
+      valueBox(
+        paste0(format(round(sum(somjaren$totaal, na.rm = TRUE)/1000000, 2), decimal.mark = ",", big.mark = " ", small.mark = " ", small.interval = 3)),
+        subtitle= paste0("Revenue from ", min(input$Yearrevline), " until ", max(input$Yearrevline), " in million"), 
+        icon = icon("dollar-sign"), color = 'red'
+      )
+    }
+  })
+  
+  output$frcashbox <- renderValueBox({
+    if (sortofgraph() == TRUE) {
+      valueBox(
+        paste0(format(round(sum(Free_cashflow$`free cash flow`[Free_cashflow$Year == input$Yearrev], na.rm = TRUE)/1000000, 2), decimal.mark = ",", big.mark = " ", small.mark = " ", small.interval = 3)),
+        subtitle = paste0("Free cashflow ", input$Yearrev, " in million"), 
+        icon = icon("dollar-sign"), color = 'red'
+      )
+    }
+    else {
+      somjaren <- Free_cashflow %>% filter(Year >= min(input$Yearrevline) & Year <= max(input$Yearrevline)) %>% group_by(Year) %>% summarize(totaal = sum(`free cash flow`, na.rm = TRUE))
+      
+      valueBox(
+        paste0(format(round(sum(somjaren$totaal, na.rm = TRUE)/1000000, 2), decimal.mark = ",", big.mark = " ", small.mark = " ", small.interval = 3)),
+        subtitle = paste0("Free cashflow from ", min(input$Yearrevline), " until ", max(input$Yearrevline), " in million"), 
+        icon = icon("dollar-sign"), color = 'red'
+      )
+    }
+  })
+  
+  output$grprbox <- renderValueBox({
+    if (sortofgraph() == TRUE) {
+      valueBox(
+        paste0(format(round(sum(Gross_profit$`Automotive gross profit GAAP`[Gross_profit$Year == input$Yearrev], na.rm = TRUE)/1000000, 2), decimal.mark = ",", big.mark = " ", small.mark = " ", small.interval = 3)), 
+        subtitle = paste0("Gross profit ", input$Yearrev, " in million"),  
+        icon = icon("piggy-bank"), color = 'red'
+      )
+    }
+    else {
+      somjaren <- Gross_profit %>% filter(Year >= min(input$Yearrevline) & Year <= max(input$Yearrevline)) %>% group_by(Year) %>% summarize(totaal = sum(`Automotive gross profit GAAP`, na.rm = TRUE))
+      valueBox(
+        paste0(format(round(sum(somjaren$totaal, na.rm = TRUE)/1000000, 2), decimal.mark = ",", big.mark = " ", small.mark = " ", small.interval = 3)),
+        subtitle = paste0("Gross profit from ", min(input$Yearrevline), " until ", max(input$Yearrevline), " in million"),  
+        icon = icon("piggy-bank"), color = 'red'
+      )
+    }
+  })
+  
+  
+  output$colrev <- renderPlot({
+    if (sortofgraph() == TRUE) {
+      
+      # generate bins based on input$bins from ui.R
+      x    <- Revenue$Year
+      Yearrev <- seq(min(x), max(x), length.out = input$Yearrev)
+      
+      revvar <- revenue(input$Yearrev, Revenue)
+      
+      revvar %>% ggplot(aes(x = Quarter, y = `Automotive Revenues Tesla`/1000000, fill= Quarter))+ geom_col(position="dodge") + 
+        labs(title = input$Yearrev, y = 'Automotive revenue')  + geom_text(aes(label = `Automotive Revenues Tesla`/1000000), vjust = -0.5 ) +
+        scale_y_continuous(limits = c(0, 8000), breaks = seq(0,8000, by= 1000)) 
+      
+      
+    }
+    else {
+      y    <- Revenue$Year
+      Yearrevline <- seq(min(y), max(y))
+      revvar <- Revenue %>% filter(Year >= min(input$Yearrevline) & Year <= max(input$Yearrevline)) %>% group_by(Year) %>% 
+        mutate("totaal" = sum(`Automotive Revenues Tesla`, na.rm = TRUE)/1000000) %>% select(Year, totaal)%>% distinct()
+      
+      revvar %>% ggplot(aes(x = Year , y = totaal))+ geom_line() + geom_point() + geom_text(aes(label = format(round(totaal, 2), decimal.mark = ",", big.mark = " ", small.mark = " ", small.interval = 3)), vjust = -0.5 ) +
+        labs(y = 'Automotive revenue') +
+        scale_y_continuous(limits = c(0, 21000), breaks = seq(0, 21000, by = 5000))
+    }
+  })
+  
+  output$colfrcash <- renderPlot({
+    if (sortofgraph() == TRUE) {
+      freecashvar <- revenue(input$Yearrev, Free_cashflow)
+      
+      
+      freecashvar %>% ggplot(aes(x= Quarter, y= `free cash flow`/1000000, fill = Quarter)) + 
+        geom_col(position="dodge") + geom_text(aes(label = `free cash flow`/1000000), vjust = -0.5 ) +
+        labs(title = input$Yearrev, y = 'Free cash flow') + 
+        scale_y_continuous(limits = c(-1500, 1500), breaks = seq(-1500,1500, by = 500))
+      
+    }
+    else {
+      freecashvar <-  Free_cashflow %>% filter(Year >= min(input$Yearrevline) & Year <= max(input$Yearrevline)) %>% group_by(Year) %>% 
+        mutate("totaal" = sum(`free cash flow`, na.rm = TRUE)/1000000) %>% select(Year, totaal)%>% distinct()
+      
+      freecashvar %>% ggplot(aes(x = Year , y = totaal))+ 
+        geom_line() + geom_point() + geom_text(aes(label = format(round(totaal, 2), decimal.mark = ",", big.mark = " ", small.mark = " ", small.interval = 3)), vjust = -0.5 ) +
+        labs(y = 'Free cash flow')  + scale_y_continuous(limits = c(-4000, 1300), breaks = seq(-4000, 1300, by= 1000))
+      
+    }
+  })
+  
+  output$colgrpr <- renderPlot({
+    if (sortofgraph() == TRUE) {
+      grossprofitvar <- revenue(input$Yearrev, Gross_profit) 
+      
+      grossprofitvar %>% 
+        ggplot(aes(x = Quarter, y = `Automotive gross profit GAAP`/1000000, fill= Quarter)) + 
+        geom_col(position="dodge") +  geom_text(aes(label = `Automotive gross profit GAAP`/1000000), vjust = -0.5 ) +
+        labs(title = input$Yearrev, y= 'Gross profit') + 
+        scale_y_continuous(limits = c(0,2500), breaks = seq(0,2500, by= 500))
+      
+    }
+    else {
+      grossprofitvar <- Gross_profit %>% filter(Year >= min(input$Yearrevline) & Year <= max(input$Yearrevline)) %>% group_by(Year) %>% 
+        mutate("totaal" = sum(`Automotive gross profit GAAP`, na.rm = TRUE)/1000000) %>% select(Year, totaal)%>% distinct() 
+      
+      grossprofitvar %>% ggplot(aes(x = Year , y = totaal)) + 
+        geom_line() + geom_point() + geom_text(aes(label = format(round(totaal, 2), decimal.mark = ",", big.mark = " ", small.mark = " ", small.interval = 3)), vjust = -0.5 ) +
+        labs(y = "Gross profit")  + scale_y_continuous(limits = c(0, 5000), breaks = seq(0, 5000, by= 1000)) 
+      
+    }
+  })
+  
+  output$colgrmar <- renderPlot({
+    if (sortofgraph() == TRUE) {
+      grossmarginvar <- revenue(input$Yearrev, Gross_Margin)
+      
+      grossmarginvar %>% 
+        ggplot(aes(x = Quarter, y = `Gross margin Automotive GAAP`, fill= Quarter)) + 
+        geom_col(position="dodge") + geom_text(aes(label = `Gross margin Automotive GAAP`), vjust = -0.5 ) +
+        labs(title = input$Yearrev, y= 'Gross margin') + 
+        scale_y_continuous(limits = c(0,30), breaks = seq(0, 30, by= 5))
+      
+    }
+    else {
+      grossmarginvar <- Gross_Margin %>% filter(Year >= min(input$Yearrevline) & Year <= max(input$Yearrevline)) %>% group_by(Year) %>% 
+        mutate("totaal" = sum(`Gross margin Automotive GAAP`, na.rm = TRUE)) %>% select(Year, totaal)%>% distinct()
+      
+      grossmarginvar %>% ggplot(aes(x = Year , y = totaal)) + 
+        geom_line() + geom_point() + geom_text(aes(label = format(round(totaal, 2), decimal.mark = ",", big.mark = " ", small.mark = " ", small.interval = 3)), vjust = -0.5 ) +
+        labs(y = "Gross margin")  + scale_y_continuous(limits = c(0, 100), breaks = seq(0, 100, by= 10))
+      
+    }
+  })
+  
+  #Uitbreiding naar de EU
+  checkeurope <- reactive({input$Europe})
+  output$colpascar <- renderPlot({
+    if (checkeurope() == 2) {
+      countriespasscarvar <- countriesafpassengercars %>% filter(Country == input$EUoptions, Fuel %in% input$EUcheck)
+      
+      countriespasscarvar %>% 
+        ggplot(aes(x = Year, y = waardes, fill = Fuel)) + 
+        geom_col(position = "dodge") + 
+        labs(title = input$EUoptions, y = '')  + 
+        scale_y_continuous(limits = c(0, 3600000), breaks = seq(0,4000000, by= 500000)) +
+        scale_x_continuous(limits= c(min(countriesafpassengercars$Year), max(countriesafpassengercars$Year)) , breaks = seq(min(countriesafpassengercars$Year), max(countriesafpassengercars$Year), by = 1))
+      
+    }
+    else {
+      countriespasscarvar <- countriesafpassengercars %>% filter(Fuel %in% input$EUcheck, Year == input$YearEU)
+      
+      countriespasscarvar %>% 
+        ggplot(aes(x = Country, y = waardes, fill = Fuel ))+ 
+        geom_col(position = "dodge") + 
+        labs(title = input$YearEU, y = '')  + 
+        scale_y_continuous(limits = c(0, 3600000), breaks = seq(0,4000000, by= 500000)) + 
+        coord_flip()
+      
+    }
+  })
+  output$colinfr <- renderPlot({
+    if (checkeurope() == 2) {
+      countriesinfrvar <- countriesafinfrastructure %>% filter(Country == input$EUoptions, Fuel %in% input$EUcheckinfr)
+      
+      countriesinfrvar %>% 
+        ggplot(aes(x = Year, y = waardes, fill = Fuel))+ 
+        geom_col(position = "dodge") + 
+        labs(title = input$EUoptions, y = '')  + 
+        scale_y_continuous(limits = c(0,65000), breaks = seq(0,65000, by= 5000)) +
+        scale_x_continuous(limits = c(min(countriesafinfrastructure$Year), max(countriesafinfrastructure$Year)), breaks = seq(min(countriesafinfrastructure$Year), max(countriesafinfrastructure$Year), by = 1))
+      
+    }
+    else {
+      countriesinfrvar <- countriesafinfrastructure %>% filter(Fuel %in% input$EUcheckinfr, Year == input$YearEU)
+      
+      countriesinfrvar %>% 
+        ggplot(aes(x = Country, y = waardes, fill = Fuel))+ 
+        geom_col(position = "dodge") + 
+        labs(title = input$YearEU, y = '')  + 
+        scale_y_continuous(limits = c(0, 65000), breaks = seq(0,65000, by= 5000)) +
+        coord_flip()
+      
+    }
+  })
+  output$distPlot <- renderPlot({
+    teslamap <- function(inputjaar, df) {
+      if (inputjaar == "2013") {
+        teslamap <- df %>% filter(df$jaar == inputjaar, df$waarde > 0)
+      }
+      else {
+        teslamap <- df %>% filter(df$jaar == c(inputjaar, (as.numeric(inputjaar) - 1)), df$waarde > 0)  
+      }
+      return(teslamap)
+    }
+    
+    teslamapvar <- teslamap(input$teslajaar, tesla.eu.map)
+    gg <- teslamapvar %>% ggplot() + geom_map(dat = tesla.eu.map, map = tesla.eu.map, aes(map_id = region), fill = "white", color="black")
+    if (input$teslajaar == "2013") {
+      gg <- gg + geom_map(map = tesla.eu.map, aes(map_id = region, fill = jaar), colour = "black")
+    }
+    else {
+      gg <- gg + geom_map(map = tesla.eu.map, aes(map_id = region, fill = jaar), colour = "black")
+    }
+    gg <- gg + expand_limits(x = tesla.eu.map$long, y = tesla.eu.map$lat)
+    gg <- gg + theme(axis.title = element_blank(),
+                     axis.text = element_blank(),
+                     axis.ticks = element_blank(),
+                     panel.grid.major = element_blank(),
+                     panel.grid.minor = element_blank(),
+                     panel.background = element_blank(),
+                     legend.position = "none",
+                     panel.border = element_blank(),
+                     strip.background = element_rect(fill = 'white', colour = 'white')) + scale_fill_manual( values = c("tomato", "skyblue"))
+    gg
+    
+  })
   
 })
 
--- Dashboard/ui.R
+++ Dashboard/ui.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(readxl)
+library(readr)
 library(maps)
 library(ggplot2)
 library(tidyr)
@@ -49,6 +50,20 @@ Data <- data %>% gather(January:December, key=  "Month", value="Sales") %>% muta
 Data$Month <- as.integer(Data$Month)
 Data$Year <- as.factor(Data$Year) 
 
+
+#Lien
+
+#financieel tabblad
+Revenue <- read_xlsx("data/Revenue-gross margin-gross profit worldwide 2015-2020.xlsx", sheet = "Revenues (automotive)", col_types = c("numeric", "text", "numeric", "numeric"))
+Gross_Margin <- read_xlsx("Data/Revenue-gross margin-gross profit worldwide 2015-2020.xlsx", sheet = "Gross margin", col_types = c("numeric", "text", "numeric", "numeric"))
+Gross_profit <- read_xlsx("Data/Revenue-gross margin-gross profit worldwide 2015-2020.xlsx", sheet = "Gross profit", col_types = c("numeric", "text", "numeric", "numeric", "numeric"))
+Free_cashflow <- read_xlsx("Data/Tesla's free cash flow by quarter 2020 world wide.xlsx", skip = 3 , sheet = "Data", col_types = c("numeric", "text", "numeric"))
+
+#uitbreiding in europa tabblad
+countriesafpassengercars <- read_xlsx("Data/Countries overview of af passenger cars.xlsx", skip = 2 , col_types = c("numeric", "text", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+
+
+
 # Define UI for application that draws a map
 shinyUI(
   dashboardPage(skin = 'red',
@@ -60,10 +75,13 @@ shinyUI(
                              menuSubItem("Competition", tabName = "Competition")),
                     menuItem("Growth", tabName = "Growth", newTab = T, menuSubItem("Sales per segment", tabName = "Salespersegment"), menuSubItem("Sales per fuel type", tabName = "fueltype")),
                     menuItem("Customers", tabName = "Customers", newTab = T, menuSubItem("Purchase process", tabName = "Purchaseprocess")),
-                    menuItem("Sales", tabName = "Sales", newTab =T, menuSubItem("Periodic analysis", tabName = "Periodic"))
+                    menuItem("Sales", tabName = "Sales", newTab =T, menuSubItem("Periodic analysis", tabName = "Periodic")),
+                    menuItem("Finance", tabName = "Omzet"),
+                    menuItem("Expansion in Europe", tabName = "EU")
                   )),
                 dashboardBody(
                   tabItems(
+                    #Caro
                     tabItem(
                       tabName = "Map",  
                       leafletOutput("mymap"), dataTableOutput("table01")),
@@ -235,8 +253,82 @@ shinyUI(
                                     multiple = TRUE,
                                     selected = c("2019","2020"))
                       )
-                    )
+                    ),
+                    #financiele tabblad
+                    tabItem(tabName = "Omzet",
+                            h2("Financial numbers worldwide, based on automotive sector"),
+                            fluidRow(
+                              valueBoxOutput("revbox"),
+                              valueBoxOutput("frcashbox"),
+                              valueBoxOutput("grprbox")
+                            ),    
+                            #Grafieken voor financiele cijfers
+                            fluidRow(
+                              box(title = "Car revenue",
+                                  "In million",
+                                  solidHeader = T, status="danger", plotOutput("colrev")),
+                              box(title = "Free cashflow", 
+                                  "In million", solidHeader = T, status="danger", plotOutput(("colfrcash")))
+                            ),
+                            fluidRow(
+                              box(title = "Gross profit", 
+                                  "In million", solidHeader = T, status="danger", plotOutput("colgrpr")),
+                              box(title = "Gross margin", 
+                                  "In percentage", solidHeader = T, status="danger", plotOutput("colgrmar")),
+                              #aanpasbare waardes
+                              box(title = "Make changes to the graphs (Quarterly)",
+                                  solidHeader = T, status="danger", sliderInput(inputId = "Yearrev", 
+                                                                                label = "Choose year to give the quarters of",
+                                                                                min = min(Revenue$Year),
+                                                                                max = max(Revenue$Year),
+                                                                                value = 2020),
+                                  checkboxInput("Quarterly", "Quarterly overview", value = FALSE)
+                              ),
+                              box(title = "Make changes to the graph (Yearly)", solidHeader = T, status="danger",
+                                  sliderInput(inputId = "Yearrevline", 
+                                              label = "Choose the range of years to appear",
+                                              min = min(Revenue$Year),
+                                              max = max(Revenue$Year),
+                                              value = c(min(Revenue$Year),max(Revenue$Year)))
+                              )
+                            )
+                            
+                    ),
+                    tabItem(tabName = "EU",
+                            fluidRow(
+                              box(title = "AF passenger cars",
+                                  "Total fleet of passenger cars per alternative fuel (AF)", solidHeader = T, status="danger", plotOutput("colpascar"),
+                                  checkboxGroupInput("EUcheck", "Choose the fuels for Europe or per country", c('BEV', 'CNG', 'H2', 'LNG', 'LPG', 'PHEV', 'Total'), 
+                                                     selected = c('BEV', 'CNG', 'H2', 'LNG', 'LPG', 'PHEV'))),
+                              box(title = "AF infrastructure",
+                                  "Total number of alternative fuel (AF) infrastructure per type of fuel",  solidHeader = T, status="danger",plotOutput("colinfr"),
+                                  checkboxGroupInput("EUcheckinfr", "Choose the fuels for Europe or per country", c('Electricity', 'H2', 'Natural Gas', 'LPG', 'Total'), 
+                                                     selected = c('Electricity', 'H2', 'Natural Gas', 'LPG')))
+                            ),
+                            fluidRow(
+                              box(title = "Choose your options", solidHeader = T, status="danger", 
+                                  radioButtons("Europe", label= "Choose what you want to see on the graph", choices = list("Europe" = 1, "Per chosen country" = 2)),
+                                  sliderInput(inputId = "YearEU", 
+                                              label = "Choose year for the values in Europe",
+                                              min = min(countriesafpassengercars$Year),
+                                              max = max(countriesafpassengercars$Year),
+                                              value = 2020),
+                                  
+                                  selectInput("EUoptions", "Choose a country of Europe", 
+                                              c('Ausria', 'Belgium', 'Bulgaria', 'Croatia', 'Cyprus', 'Czech Republic', 'Denmark', 'Estonia',
+                                                'Finland', 'France', 'Germany', 'Greece', 'Hungria', 'Ireland', 'Italy', 'Latvia', 'Lithuania',
+                                                'Luxembourg', 'Malta', 'Netherlands', 'Poland', 'Portugal', 'Romania', 'Slovakia', 'Slovenia',
+                                                'Spain', 'Sweden'), selected = "Belgium")
+                              ),
+                              box(title = "Tesla sales in Europe per year", solidHeader = T, status="danger", 
+                                  selectInput(inputId = "teslajaar",
+                                              label = "choose the year you want to see (blue is new that year)",
+                                              choices = list("2013", "2014", "2015", "2016", "2017", "2018", "2019")),
+                                  plotOutput("distPlot")))
+                          )
+                            
+                            
+                    )  
                   )
                 )
   )
-)
