34f3c08a044416468fb15c0c88f72d7d06c7ba8f
Pieter-van-Meer <vanmeerpieter@gmail.com>
2020-11-08 10:02:48 +0100

    v21

--- Dashboard_Pieter/server.R
+++ Dashboard_Pieter/server.R
@@ -3,39 +3,50 @@
 # Libraries
 library(shiny)
 library(shinydashboard)
+library(tidyverse)
+library(readxl)
 
 # Static ggplots
-# Loyalty
-loyalty_per_brand_1
-loyalty_per_brand_2 <-loyalty_per_brand_1 %>% arrange(desc(Percentage)) 
-loyalty_per_brand_2
-
-
-loyalty_per_brand_plot <- ggplot(loyalty_per_brand_1, aes(x = Percentage,
-                                                       y = Merk)) + 
-                          geom_col()
-loyalty_per_brand_plot
-
-loyalty_per_brand_luxury <- loyalty_per_brand_2 %>% filter(`Type of brand` == "Luxury")
-loyalty_per_brand_luxury
-
-loyalty_per_brand_luxury_plot <- loyalty_per_brand_luxury %>%
-  ggplot(aes(x=Percentage,
-             y=Merk)) +
-  geom_col()
-loyalty_per_brand_luxury_plot
+# # Loyalty
+# loyalty_per_brand_1
+# loyalty_per_brand_2 <-loyalty_per_brand_1 %>% arrange(desc(Percentage)) 
+# loyalty_per_brand_2
+# 
+# 
+# loyalty_per_brand_plot <- ggplot(loyalty_per_brand_1, aes(x = Percentage,
+#                                                        y = Merk)) +
+#                           geom_col()
+# loyalty_per_brand_plot
+# 
+# loyalty_per_brand_luxury <- loyalty_per_brand_2 %>% filter(`Type of brand` == "Luxury")
+# loyalty_per_brand_luxury
+# 
+# loyalty_per_brand_luxury_plot <- loyalty_per_brand_luxury %>%
+#   ggplot(aes(x=Percentage,
+#              y=Merk)) +
+#   geom_col()
+# loyalty_per_brand_luxury_plot
 
 # Shiny Server
 shinyServer(function(input, output){
   
-  # Growth
+  # Growth template
   output$histogram_growth <- renderPlot({
     hist(faithful$eruptions, breaks = input$bins_growth)
   })
   
-  # Loyalty
+  # Loyalty template
   output$histogram_loyalty <- renderPlot({
     hist(faithful$eruptions, breaks = input$bins_loyalty)
+  })
+  
+  # Real loyalty
+  output$loyalty_col <- renderPlot({
+    loyalty_per_brand_2 <-loyalty_per_brand_1 %>% arrange(desc(Percentage))
+    loyalty_per_brand_plot <- ggplot(loyalty_per_brand_1, aes(x = Percentage,
+                                                              y = Merk)) +
+      geom_col()
+    loyalty_per_brand_plot
   })
   
 })
