e7bb779d39f60149ee25e07f34079cd5d648bab0
Pieter-van-Meer <vanmeerpieter@gmail.com>
2020-11-08 08:35:56 +0100

    v19 (prutsen met dplyr)

--- Dashboard_Pieter/server.R
+++ Dashboard_Pieter/server.R
@@ -4,13 +4,26 @@
 library(shiny)
 library(shinydashboard)
 
-# Static ggplot
-loyalty_per_brand_1 %>%
-arrange(desc(Percentage)) %>% 
-ggplot(aes(x = Percentage,
-           y = Merk)) + 
-  geom_col()
+# Static ggplots
+# Loyalty
+loyalty_per_brand_1
+loyalty_per_brand_2 <-loyalty_per_brand_1 %>% arrange(desc(Percentage)) 
+loyalty_per_brand_2
 
+
+loyalty_per_brand_plot <- ggplot(loyalty_per_brand_1, aes(x = Percentage,
+                                                       y = Merk)) + 
+                          geom_col()
+loyalty_per_brand_plot
+
+loyalty_per_brand_luxury <- loyalty_per_brand_2 %>% filter(`Type of brand` == "Luxury")
+loyalty_per_brand_luxury
+
+loyalty_per_brand_luxury_plot <- loyalty_per_brand_luxury %>%
+  ggplot(aes(x=Percentage,
+             y=Merk)) +
+  geom_col()
+loyalty_per_brand_luxury_plot
 
 # Shiny Server
 shinyServer(function(input, output){
