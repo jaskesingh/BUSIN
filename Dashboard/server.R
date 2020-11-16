7e6d26160dc91a8efafb095096bb2a79059ff8be
Jaske Singh <jaske163@gmail.com>
2020-11-16 10:21:18 +0100

    Trend added to stock chart

--- Dashboard/server.R
+++ Dashboard/server.R
@@ -622,6 +622,7 @@ shinyServer(function(input, output, session) {
     
     p <- TSLA %>% ggplot(aes(date , close)) + geom_line() +
       labs(title = "TSLA stock evolution", y = "Closing Price", x = "") + 
+      stat_smooth(method = 'lm', se = FALSE, aes(color = 'Trend')) +
       theme_tq()
     
     ggplotly(p)
