4e8422a05b059d71634f75858b150df42ae9e0d6
Jaske Singh <jaske163@gmail.com>
2020-11-20 15:36:56 +0100

    problem solved SURVEY

--- Dashboard/server.R
+++ Dashboard/server.R
@@ -400,7 +400,7 @@ shinyServer(function(input, output, session) {
     output$view <- renderPlotly({
       f3 <- eusurvey %>% filter(Country == input$incountry, Income_group == input$incomegr)
       p3 <- f3 %>% ggplot(aes(Income_group)) + 
-        geom_bar(aes(fill = buy_electric), position = "dodge") +
+        geom_bar(aes(fill = as.logical(buy_electric)), position = "dodge") +
         labs(y = "Number of respondents", fill = "Buy EV") +
         theme(axis.text.x = element_text(angle = 60, hjust = 1))
       ggplotly(p3)
@@ -409,7 +409,7 @@ shinyServer(function(input, output, session) {
     output$employ <- renderPlotly({
       f1 <- eusurvey %>% filter(Employment_status %in% input$estatus)
       p1 <- f1 %>% ggplot(aes(Employment_status)) +
-        geom_bar(aes(fill = buy_electric), position = "dodge") +
+        geom_bar(aes(fill = as.logical(buy_electric)), position = "dodge") +
         labs(y = "Number of respondents", fill = "Buy EV") +
         theme(axis.text.x = element_text(angle = 60, hjust = 1))
       ggplotly(p1) %>% 
@@ -422,7 +422,7 @@ shinyServer(function(input, output, session) {
     output$ggcountry <- renderPlotly({
       f2 <- eusurvey %>% filter(Country %in% input$gcountry)
       p2 <- f2 %>% ggplot(aes(Gender)) + 
-        geom_bar(aes(fill = buy_electric), position = "dodge") +
+        geom_bar(aes(fill = as.logical(buy_electric)), position = "dodge") +
         facet_wrap(~Country) + 
         labs(y = "Number of respondents", fill = "Buy EV")
       ggplotly(p2) %>% 
@@ -435,7 +435,7 @@ shinyServer(function(input, output, session) {
     output$plan <- renderPlotly({
       f4 <- eusurvey %>% filter (Country %in% input$carplancountry)
       p4 <- f4 %>% ggplot(aes(Plan_to_purchase_vehicle)) + 
-        geom_bar(aes(fill = buy_electric), position = "dodge") + 
+        geom_bar(aes(fill = as.logical(buy_electric)), position = "dodge") + 
         labs(y = "Number of respondents", x = "Plan to buy car", fill = "Buy EV") +
         theme(axis.text.x = element_text(angle = 60, hjust = 1))
       ggplotly(p4)
